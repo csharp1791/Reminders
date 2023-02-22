eb7b0cb0db79b92e62c9ea0edb9d321efc8dca84
Clifford Sharp <csharpusa@gmail.com>
2023-02-22 11:11:53 -0600

    Initial Commit

---
+++ RemindersApp/RemindersAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RemindersAppApp.swift
+//  RemindersApp
+//
+//  Created by Cliff Sharp on 2/22/23.
+//
+
+import SwiftUI
+
+@main
+struct RemindersAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
