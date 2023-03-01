067fb89c88de7ec4bb8988afc4920467072baded
Clifford Sharp <csharpusa@gmail.com>
2023-03-01 17:23:25 -0600

    Counts on HomeView are updating correctly

--- RemindersApp/Services/ReminderService.swift
+++ RemindersApp/Services/ReminderService.swift
@@ -10,53 +10,59 @@ import CoreData
 import UIKit
 
 class ReminderService {
-  
-  static var viewContext: NSManagedObjectContext {
-    CoreDataProvider.shared.persistentContainer.viewContext
-  }
-  
-  static func save() throws {
-    try viewContext.save()
-  }
-  
-  static func saveMyList(_ name: String, _ color: UIColor) throws {
-    let myList = MyList(context: viewContext)
-    myList.name = name
-    myList.color = color
-    try save()
-  }
-  
-  static func updateReminder(reminder: Reminder, editConfig: ReminderEditConfig) throws -> Bool {
     
-    let reminderToUpdate = reminder
+    static var viewContext: NSManagedObjectContext {
+        CoreDataProvider.shared.persistentContainer.viewContext
+    }
     
-    reminderToUpdate.isCompleted = editConfig.isCompleted
-    reminderToUpdate.title = editConfig.title
-    reminderToUpdate.notes = editConfig.notes
-    reminderToUpdate.reminderDate = editConfig.hasDate ? editConfig.reminderDate : nil
-    reminderToUpdate.reminderTime = editConfig.hasTime ? editConfig.reminderTime : nil
+    static func save() throws {
+        try viewContext.save()
+    }
     
-    try save()
-    return true
-  }
-  
-  static func deleteReminder(_ reminder: Reminder) throws {
-    viewContext.delete(reminder)
-    try save()
-  }
-  
-  static func saveReminderToMyList(myList: MyList, reminderTitle: String) throws {
-    let reminder = Reminder(context: viewContext)
-    reminder.title = reminderTitle
-    myList.addToReminders(reminder)
-    try save()
-  }
-  
-  static func getRemindersByList(myList: MyList) -> NSFetchRequest<Reminder> {
-    let request = Reminder.fetchRequest()
-    request.sortDescriptors = []
-    request.predicate = NSPredicate(format: "list = %@ AND isCompleted = false", myList)
-    return request
-  }
-  
+    static func saveMyList(_ name: String, _ color: UIColor) throws {
+        let myList = MyList(context: viewContext)
+        myList.name = name
+        myList.color = color
+        try save()
+    }
+    
+    static func updateReminder(reminder: Reminder, editConfig: ReminderEditConfig) throws -> Bool {
+        
+        let reminderToUpdate = reminder
+        reminderToUpdate.isCompleted = editConfig.isCompleted
+        reminderToUpdate.title = editConfig.title
+        reminderToUpdate.notes = editConfig.notes
+        reminderToUpdate.reminderDate = editConfig.hasDate ? editConfig.reminderDate: nil
+        reminderToUpdate.reminderTime = editConfig.hasTime ? editConfig.reminderTime: nil
+        
+        try save()
+        return true
+    }
+    
+    static func deleteReminder(_ reminder: Reminder) throws {
+        viewContext.delete(reminder)
+        try save()
+    }
+    
+    static func getRemindersBySearchTerm(_ searchTerm: String) -> NSFetchRequest<Reminder> {
+        let request = Reminder.fetchRequest()
+        request.sortDescriptors = []
+        request.predicate = NSPredicate(format: "title CONTAINS[cd] %@", searchTerm)
+        return request
+    }
+    
+    static func saveReminderToMyList(myList: MyList, reminderTitle: String) throws {
+        let reminder = Reminder(context: viewContext)
+        reminder.title = reminderTitle
+        myList.addToReminders(reminder)
+        try save()
+    }
+    
+    static func getRemindersByList(myList: MyList) -> NSFetchRequest<Reminder> {
+        let request = Reminder.fetchRequest()
+        request.sortDescriptors = []
+        request.predicate = NSPredicate(format: "list = %@ AND isCompleted = false", myList)
+        return request
+    }
+    
 }
--- RemindersApp/Views/HomeView.swift
+++ RemindersApp/Views/HomeView.swift
@@ -12,24 +12,44 @@ struct HomeView: View {
   @FetchRequest(sortDescriptors: [])
   private var myListResults: FetchedResults<MyList>
   
+  @FetchRequest(sortDescriptors: [])
+  private var searchResults: FetchedResults<Reminder>
+  
+  @State private var search: String = ""
   @State private var isPresented: Bool = false
+  @State private var searching: Bool = false
+  
+  private var reminderStatsBuilder = ReminderStatsBuilder()
+  @State private var reminderStatsValues = ReminderStatsValues()
   
   var body: some View {
     NavigationStack {
       VStack {
-        
-        MyListsView(myLists: myListResults)
-        
-        //Spacer()
-        
-        Button {
-          isPresented = true
-        } label: {
-          Text("Add List")
-            .frame(maxWidth: .infinity, alignment: .trailing)
-            .font(.headline)
-        }.padding()
-      }.sheet(isPresented: $isPresented) {
+        ScrollView {
+          
+          HStack {
+            ReminderStatsView(icon: "calendar", title: "Today", count: reminderStatsValues.todayCount)
+            ReminderStatsView(icon: "tray.circle.fill", title: "All", count: reminderStatsValues.allCount)
+          }
+          HStack {
+            ReminderStatsView(icon: "calendar.circle.fill", title: "Scheduled", count: reminderStatsValues.scheduledCount)
+            ReminderStatsView(icon: "checkmark.circle.fill", title: "Completed", count: reminderStatsValues.completedCount)
+          }
+          
+          MyListsView(myLists: myListResults)
+          
+          //Spacer()
+          
+          Button {
+            isPresented = true
+          } label: {
+            Text("Add List")
+              .frame(maxWidth: .infinity, alignment: .trailing)
+              .font(.headline)
+          }.padding()
+        }
+      }
+      .sheet(isPresented: $isPresented) {
         NavigationView {
           AddNewListView { name, color in
             do {
@@ -40,8 +60,22 @@ struct HomeView: View {
           }
         }
       }
-    }
-    .padding()
+      .listStyle(.plain)
+      .onChange(of: search, perform: { searchTerm in
+        searching = !searchTerm.isEmpty ? true: false
+        searchResults.nsPredicate = ReminderService.getRemindersBySearchTerm(search).predicate
+      })
+      .overlay(alignment: .center, content: {
+        ReminderListView(reminders: searchResults)
+          .opacity(searching ? 1.0: 0.0)
+      })
+      .onAppear {
+        reminderStatsValues = reminderStatsBuilder.build(myListResults: myListResults)
+      }
+      .padding()
+      .navigationTitle("Reminders")
+    }.searchable(text: $search)
+    
   }
 }
 
